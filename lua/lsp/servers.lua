4835ed70387eaa7136bf9094d58cfec5060249e3
Gaubbe <gabriel12125@gmail.com>
2022-10-06 14:53:39 -0400

    Added: Basic remaps

--- lua/lsp/servers.lua
+++ lua/lsp/servers.lua
@@ -7,13 +7,30 @@ local lspmap = function(mode, rhs, lhs, opts)
 	vim.keymap.set(mode, "<Leader>l" .. rhs, lhs, opts)
 end
 
-local gopts = { silent = true, noremap = true }
+local opts = { silent = true, noremap = true }
 
-lspmap("n", "f", vim.diagnostic.open_float, gopts)
-lspmap("n", "n", vim.diagnostic.goto_next, gopts)
-lspmap("n", "N", vim.diagnostic.goto_prev, gopts)
+lspmap("n", "f", vim.diagnostic.open_float, opts)
+lspmap("n", "n", vim.diagnostic.goto_next, opts)
+lspmap("n", "N", vim.diagnostic.goto_prev, opts)
 
-lspconfig.sumneko_lua.setup{
+local on_attach = function(_, bufnr)
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+	local bufopts = { silent = true, noremap = true, buffer = bufnr }
+	lspmap('n', 'd', vim.lsp.buf.definition, bufopts)
+	lspmap('n', 'D', vim.lsp.buf.declaration, bufopts)
+	lspmap('n', 'h', vim.lsp.buf.hover, bufopts)
+	lspmap('n', 'i', vim.lsp.buf.implementation, bufopts)
+	lspmap('n', 's', vim.lsp.buf.signature_help, bufopts)
+	lspmap('n', 't', vim.lsp.buf.type_definition, bufopts)
+	lspmap('n', 'r', vim.lsp.buf.rename, bufopts)
+	lspmap('n', 'a', vim.lsp.buf.code_action, bufopts)
+	lspmap('n', 'R', vim.lsp.buf.references, bufopts)
+	lspmap('n', 'F', function() vim.lsp.buf.format{ async = true } end, bufopts)
+end
+
+lspconfig.sumneko_lua.setup {
+	on_attach = on_attach,
 	settings = {
 		Lua = {
 			diagnostics = {
