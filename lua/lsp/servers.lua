b66e0ec51828b29551332178d74ef113820b3d4c
Gaubbe <gabriel12125@gmail.com>
2022-10-06 10:35:17 -0400

    Added: sumneko_lua now recognises nvim library

--- lua/lsp/servers.lua
+++ lua/lsp/servers.lua
@@ -3,5 +3,16 @@ require('mason-lspconfig').setup()
 
 local lspconfig = require('lspconfig');
 
-lspconfig.sumneko_lua.setup{}
+lspconfig.sumneko_lua.setup{
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" }
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true)
+			}
+		}
+	}
+}
 
