8458a31a935063ad7aba784ac37b6e9be3ea28c4
Gaubbe <gabriel12125@gmail.com>
2023-09-26 13:45:20 -0400

    feat: added lsp remaps

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,11 +1,14 @@
-local normalized_data_dir = vim.fs.normalize(vim.fn.stdpath("data"))
-local jdtls_bin_path = vim.fs.find("mason/bin/jdtls", {
-	path = normalized_data_dir
-})
+local mason_bin_dir = vim.fs.normalize(vim.fn.stdpath("data") .. "/mason/bin")
+local jdtls_bin_path = vim.fs.find("jdtls", {
+	path = mason_bin_dir
+})[1]
 
 local config = {
     cmd = { jdtls_bin_path },
     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+	on_attach = function (_, bufnr)
+		require('lsp.remaps').remap_for_buffer(bufnr)
+	end
 }
 require('jdtls').start_or_attach(config)
 
