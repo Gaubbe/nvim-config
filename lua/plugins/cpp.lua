869789e1a6fef2d7757a96bbb5090ccf3981953c
Gaubbe <gabriel12125@gmail.com>
2024-05-09 18:43:54 -0400

    fix: made cmake-tools platform agnostic

--- lua/plugins/cpp.lua
+++ lua/plugins/cpp.lua
@@ -1,8 +1,17 @@
 return {
 	{
 		'Civitasv/cmake-tools.nvim',
+		name = "cmake-tools",
 		config = function ()
-			require("cmake-tools").setup({})
+			local build_directory = "build/${variant:buildType}"
+
+			if vim.loop.os_uname().sysname:find("Windows") then
+				build_directory = "build\\${variant:buildType}"
+			end
+
+			require("cmake-tools").setup({
+				cmake_build_directory = build_directory
+			})
 		end
 	}
 }
