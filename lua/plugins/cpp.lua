e3255ed083a420cab828a5783e5240f5069e015f
Gaubbe <gabriel12125@gmail.com>
2024-05-15 09:35:43 -0400

    feat: only load cmake tools if cmake project

--- lua/plugins/cpp.lua
+++ lua/plugins/cpp.lua
@@ -2,6 +2,16 @@ return {
 	{
 		'Civitasv/cmake-tools.nvim',
 		name = "cmake-tools",
+		cond = function()
+			local cwd = vim.fn.getcwd()
+			local cmake_file = vim.fs.find(function (name, path)
+				return name:match("CMakeLists.txt") and path == cwd
+			end, {
+				path = cwd
+			})
+
+			return next(cmake_file) ~= nil
+		end,
 		config = function ()
 			local build_directory = "build/${variant:buildType}"
 
