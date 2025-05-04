edee61d780019cb9ee0f3cb8cc13ae75fd101259
Gaubbe <gabriel12125@gmail.com>
2025-05-04 14:28:53 -0400

    feat: added CargoMetadata command for testing

--- lua/languages/rust/cargo.lua
+++ lua/languages/rust/cargo.lua
@@ -93,12 +93,39 @@ local M = {}
 --- @field workspace_root string The root directory of the workspace
 --- @field metadata any The metadata of the workspace
 
+--- Obtains the metadata of the current workspace
+--- @param client vim.lsp.Client The LSP client to use for the root directory
+--- @return CargoMetadata # The metadata
+M.get_metadata = function (client)
+	--- @type CargoMetadata
+	local metadata
+
+	local obj = vim.system({
+		'cargo',
+		'metadata',
+		'--no-deps',
+		'--format-version',
+		'1',
+	},{
+		cwd = client.root_dir,
+	}):wait()
+
+	if obj.stdout ~= nil then
+		metadata = vim.json.decode(obj.stdout)
+	end
+
+	return metadata
+end
+
 --- Creates the cargo related user commands
 --- @param client vim.lsp.Client
 --- @param bufnr integer
 M.create_cargo_user_commands = function (client, bufnr)
 	vim.api.nvim_buf_create_user_command(bufnr, 'CargoRootDir', function ()
 		vim.print(vim.inspect(client.root_dir))
+	end, {})
+	vim.api.nvim_buf_create_user_command(bufnr, 'CargoMetadata', function ()
+		vim.print(vim.inspect(M.get_metadata(client)))
 	end, {})
 end
 
