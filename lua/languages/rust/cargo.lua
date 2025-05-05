720c538b822c11813e70c86bbbbc737cfaec1772
Gaubbe <gabriel12125@gmail.com>
2025-05-04 20:02:37 -0400

    feat: now using object oriented lua

--- lua/languages/rust/cargo.lua
+++ lua/languages/rust/cargo.lua
@@ -1,43 +1,16 @@
 local M = {}
 
---- Obtains the metadata of the current workspace
---- @param client vim.lsp.Client The LSP client to use for the root directory
---- @return CargoMetadata # The metadata
-M.get_metadata = function (client)
-	--- @type CargoMetadata
-	local metadata
-
-	local obj = vim.system({
-		'cargo',
-		'metadata',
-		'--no-deps',
-		'--format-version',
-		'1',
-	},{
-		cwd = client.root_dir,
-	}):wait()
-
-	if obj.stdout ~= nil then
-		metadata = vim.json.decode(obj.stdout, {
-			luanil = {
-				object = true,
-				array = true,
-			}
-		})
-	end
-
-	return metadata
-end
-
 --- Creates the cargo related user commands
 --- @param client vim.lsp.Client
 --- @param bufnr integer
 M.create_cargo_user_commands = function (client, bufnr)
+	local cargo = require('languages.rust.cargo.instance'):new(client.root_dir)
+
 	vim.api.nvim_buf_create_user_command(bufnr, 'CargoRootDir', function ()
 		vim.print(vim.inspect(client.root_dir))
 	end, {})
 	vim.api.nvim_buf_create_user_command(bufnr, 'CargoMetadata', function ()
-		vim.print(vim.inspect(M.get_metadata(client)))
+		vim.print(vim.inspect(cargo:get_metadata()))
 	end, {})
 end
 
---
+++ lua/languages/rust/cargo/instance.lua
@@ -0,0 +1,46 @@
+--- An instance of the cargo workspace
+--- @class CargoInstance
+--- @field public root_dir string The root directory of the cargo workspace
+local CargoInstance = {}
+
+--- Creates a new cargo instance
+--- @param root_dir string The root directory of the cargo workspace
+--- @return CargoInstance # The instance
+function CargoInstance:new(root_dir)
+	local new = setmetatable({
+		root_dir = root_dir,
+	}, self)
+	self.__index = self
+
+	return new
+end
+
+--- Obtains the metadata for this cargo project
+--- @return CargoMetadata # The metadata
+function CargoInstance:get_metadata()
+	--- @type CargoMetadata
+	local metadata
+
+	local obj = vim.system({
+		'cargo',
+		'metadata',
+		'--no-deps',
+		'--format-version',
+		'1',
+	},{
+		cwd = self.root_dir,
+	}):wait()
+
+	if obj.stdout ~= nil then
+		metadata = vim.json.decode(obj.stdout, {
+			luanil = {
+				object = true,
+				array = true,
+			}
+		})
+	end
+
+	return metadata
+end
+
+return CargoInstance
